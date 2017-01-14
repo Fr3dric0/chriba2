888e253f4c4960fd84ca3e600428d882648977cc
Fredrik F Lindhagen <fred.lindh96@gmail.com>
2017-01-14 02:32:01 +0100

    feat: Add auth-guard for admin
    Auth-guard for the admin component has been added.
    Including an authentication-service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,22 +9,30 @@ import { AppRoutingModule } from "./app-routing.module";
 import { ProjectsComponent } from './projects/projects.component';
 import { EstatesComponent } from './estates/estates.component';
 import { ERROR_DECLARATIONS } from './error';
+import { SHARED_DECLARATIONS, SHARED_PROVIDERS } from './shared';
+import { ADMIN_DECLARATIONS, ADMIN_PROVIDERS } from './admin/index';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-    ProjectsComponent,
-    EstatesComponent,
-    ...ERROR_DECLARATIONS
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        DashboardComponent,
+        ProjectsComponent,
+        EstatesComponent,
+        ...ERROR_DECLARATIONS,
+        ...SHARED_DECLARATIONS,
+        ...ADMIN_DECLARATIONS
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        AppRoutingModule
+    ],
+    providers: [
+        ...SHARED_PROVIDERS,
+        ...ADMIN_PROVIDERS
+    ],
+    bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ client/src/app/shared/auth/auth.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class AuthService {
+    private tokenKey: string = 'ch_token';
+    private authUrl: string = '/api/admin/token';
+    private authValChange = new BehaviorSubject<boolean>(false);
+    authStatus = this.authValChange.asObservable();
+
+    constructor(private http: Http) {
+        this.updateAuth();
+    }
+
+    authenticate(email: string, password: string): Promise<boolean> {
+        return new Promise((rsv, rr) => {
+            this.http.post(this.authUrl, { email, password })
+                .map(res => res.json())
+                .subscribe((data) => {
+                    localStorage.setItem(this.tokenKey, data.token);
+                    this.updateAuth();
+                    rsv(true);
+                }, err => rr(err));
+        });
+    }
+
+    /**
+     * Removes the token from localStorage, whereas the admin
+     * can no longer call protected routes, thus logging him out.
+     * */
+    logout(): void {
+        localStorage.removeItem(this.tokenKey);
+        this.updateAuth();
+    }
+
+    getToken(): string {
+        return localStorage.getItem(this.tokenKey);
+    }
+
+    /**
+     * When other services would like to
+     * send HTTP-request, a header containing a token is usually
+     * required. This method can be called to get this header
+     * @return {Headers}
+     * */
+    getAuthHeader(): Headers {
+        const headers = new Headers();
+        headers.append('Authorization', this.getToken());
+        return headers;
+    }
+
+    /**
+     * Checks if user is authenticated or not
+     * @return {boolean}
+     * */
+    authenticated(): boolean {
+        return !!localStorage.getItem(this.tokenKey); // Uses !! to force value to be a boolean
+    }
+
+    /**
+     * Updates the value in the authStatus property, which
+     * components can subscribe on. Giving them a running status
+     * on the user authentication
+     * */
+    updateAuth() {
+        this.authValChange.next(this.authenticated());
+    }
+}
