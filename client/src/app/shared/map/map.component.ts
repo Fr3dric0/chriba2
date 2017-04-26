85ebb9d5b0bee7dc0ae94a8aa09615b446b9f730
Rubenjoh <rubenjo@stud.ntnu.no>
2017-04-27 01:15:36 +0200

    Added some control customization

--- client/src/app/shared/map/map.component.ts
+++ client/src/app/shared/map/map.component.ts
@@ -10,8 +10,41 @@ import {Component, OnInit, Input } from '@angular/core';
 })
 
 export class MapComponent implements OnInit{
+  private zooming: boolean;
+  private defaultUI: boolean = false;
+  private shortcuts: boolean;
+  private streetView: boolean;
+  private scaling: boolean;
+  private typeChanging: boolean;
+
+  private mapDone: boolean = false;
+
   @Input() lat: number;
   @Input() long: number;
+  @Input() zoom: number = 9;
+  @Input() control: number = 0;
 
-  ngOnInit(){ }
+  ngOnInit(){
+    if (this.control == 0) {
+      this.zooming = true;
+      this.shortcuts = false;
+      this.streetView = false;
+      this.scaling = false;
+      this.typeChanging = false;
+    } else if (this.control == 1) {
+      this.zooming = true;
+      this.shortcuts = true;
+      this.streetView = true;
+      this.scaling = true;
+      this.typeChanging = true;
+    } else {
+      this.zooming = false;
+      this.defaultUI = true;
+      this.shortcuts = false;
+      this.streetView = false;
+      this.scaling = false;
+      this.typeChanging = false;
+    }
+    this.mapDone = true;
+  }
 }
